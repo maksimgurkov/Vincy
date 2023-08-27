a27fdac178c3a2477d86a2c57bbc9b41b05351b4
Maksim Gurkov <maksim.gurkov@gmail.com>
2023-08-27 22:25:55 +0300

    Start Project

---
+++ Vincy/App/AppDelegate.swift
@@ -0,0 +1,23 @@
+//
+//  AppDelegate.swift
+//  Vincy
+//
+//  Created by Максим Гурков on 27.08.2023.
+//
+
+import UIKit
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        window = UIWindow(frame: UIScreen.main.bounds)
+        window?.rootViewController = ViewController()
+        window?.makeKeyAndVisible()
+        return true
+    }
+
+}
+
